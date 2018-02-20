deef6cff6c32f6334805b111c14c5639d5358a25
saketarora <saketarora88@yahoo.com>
2018-02-20 08:36:13 -0800

    Unit 6 Work
    This includes Lab 8i and its corresponding Prime file

---
+++ Lab08i.java
@@ -0,0 +1,32 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.lang.Math;
+
+public class Lab08i
+{
+	public static void main ( String[] args )
+	{
+		//add test cases
+		Prime P = new Prime(24);
+		P.setPrime(24);
+		P.isPrime();
+		System.out.println(P.toString());
+		P.setPrime(7);
+		P.isPrime();
+		System.out.println(P.toString());
+		P.setPrime(100);
+		P.isPrime();
+		System.out.println(P.toString());
+		
+		
+		
+		
+		
+		
+	}	
+}
